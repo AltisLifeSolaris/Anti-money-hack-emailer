0f4ed2b94368ccae9373e940388236f1e2b7336f
Russian Volk <thekillervolk@gmail.com>
2015-12-05 00:35:37 -0700

    FirstCommit

---
+++ DbInfos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiMoneyHackEmailer
+{
+    public class DbInfos
+    {
+        public List<string> suspectedPlayers { get; set; }
+        public List<string> playerId { get; set; }
+        public List<string> databaseId { get; set; }
+        public List<string> moneyInBank { get; set; }
+        public List<string> cashOnHand { get; set; }
+
+        }
+}
